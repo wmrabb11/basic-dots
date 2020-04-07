b7bbf73b856d82104e3380d018ef5d465c7acfe5
Will Rabb <wmrabb11@gmail.com>
2020-04-07 04:31:18 -0400

    Add tmp installation

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo apt-get install tmux -y
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 cp .tmux.conf ~/.tmux.conf
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp .vimrc ~/.vimrc
